793995c5f9e38a46d97ea5ffe2ca39d3119e7701
smriti <hello.itzsmriti@gmail.com>
2025-03-21 16:05:17 +0530

    event section added

--- src/hello_frontend/src/components/Footer.jsx
+++ src/hello_frontend/src/components/Footer.jsx
@@ -4,20 +4,18 @@ import logo from "../../src/assets/images/logo.png";
 const Footer = () => {
   return (
     <footer>
-      <div class="container-lg py-5">
+      <div class="container-lg py-5 my-5">
         <div class="row">
           <div class="col-lg-6 offset-lg-3">
             <div class="contact-content text-center">
               <img src={logo} alt="logo" />
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
-                ipsum
+              Devybe enhances event transparency and trust through a credits and trust score system, fostering a community-driven ecosystem. It benefits organizers and attendees while ensuring a scalable and sustainable business model.
               </p>
               <div class="hr"></div>
-              <h6>1120 Lorem ipsum dolor sit amet, KC 179050, Chandigarh.</h6>
+              <h6>Copyright @Devybe | 2025</h6>
               <h6>
-                +01 2345 6789 12<span>|</span>+01 2345 6789 12
+               +91-9670444579
               </h6>
             </div>
           </div>
